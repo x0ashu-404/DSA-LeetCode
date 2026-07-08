f17b295ad6f06ea799ba02525161a785de11fe3d
Ashutosh <ashutosh0861@gmail.com>
2026-07-08 23:56:30 +0530

    Time: 5 ms (30.39%), Space: 12 MB (11.4%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -7,28 +7,17 @@ public:
             mp[it]++;
         }
 
-        unordered_map<char, int> rp;
-        for(auto val:ransomNote){
-            rp[val]++;
-        }
+        // unordered_map<char, int> rp;
+        // for(auto val:ransomNote){
+        //     rp[val]++;
+        // }
 
         for(int i=0; i<ransomNote.size(); i++){
-            if(rp[ransomNote[i]] > mp[ransomNote[i]]){
+            if(mp[ransomNote[i]] == 0){
                 return false;
             }
+            mp[ransomNote[i]]--;
         }
         return true;
-
-        // while(rl<ransomNote.size()){
-        //     rp[ransomNote[rl]]++;
-        //     if(rp[ransomNote[rl]] != mp[magazine[rl]]){
-        //         rl++;
-        //     }
-        // }
-        // if(rl == ransomNote.size()){
-        //     return true;
-        // }else{
-        //     return false;
-        // }
     }
 };
